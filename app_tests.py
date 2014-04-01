2a3855372f521406b27ad8feac5a22b219185e98
Ben Firshman <ben@firshman.co.uk>
2014-04-01 13:30:07 +0100

    Fix tests

--- app_tests.py
+++ app_tests.py
@@ -1,23 +1,15 @@
 import os
 import app
 import unittest
-import tempfile
 
 class HomeTestCase(unittest.TestCase):
-
     def setUp(self):
-        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
-        #app.init_db()
 
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(app.app.config['DATABASE'])
-
-    def test_property(self):
-        rv = self.app.get('/properties/EX1354')
-        assert 'Your property' in rv.data
+    def test_home(self):
+        rv = self.app.get('/')
+        assert 'hello' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
